4ce6b1224332baf3c02a5e4052694f11f9b796a9
Vincent Knauss <vbk737@gmail.com>
2022-01-14 09:12:14 -0600

    forgot to actually save last time :/

---
+++ src/physics/dynamics/constraint.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace physics::dynamics {
+
+
+
+}
--- src/physics/dynamics/world.cpp
+++ src/physics/dynamics/world.cpp
@@ -6,7 +6,20 @@
 namespace physics::dynamics {
 
 void world::step() {
-    
+    _collision_world.update();
+
+    for (auto i = 0u; i < _data.num_bodies; ++i) {
+        if (_data.mass_inverses[i] > 0) {
+            _data.velocities[i].linear += v2(0, -9.8) * timestep;
+        }
+    }
+
+    _solver.update_constraints(_collision_world);
+    _solver.pre_step(_data);
+
+    for (int i = 0; i < iterations; ++i) {
+        _solver.step(_data, timestep);
+    }
 }
 
 }
